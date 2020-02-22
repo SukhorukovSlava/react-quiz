d45128641cda6357ade246bd8939704a11746b55
slavik <vinom2008@yandex.ru>
2020-02-22 14:04:29 +0300

    Переписал компонет Quiz с классового на функциональный + правка отступов.

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,73 +1,74 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import cssCls from './Quiz.css';
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
-import FinishedQuiz from  '../../components/FinishedQuiz/FinishedQuiz';
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz';
 import Loader from "../../components/UI/Loader/Loader";
 import {connect} from "react-redux";
 import {
-    answerClickHandler,
-    fetchQuizById,
-    retryQuiz,
+  answerClickHandler,
+  fetchQuizById,
+  retryQuiz,
 } from "../../store/actions/quiz";
 
-class Quiz extends React.Component {
+const Quiz = props => {
 
-    componentDidMount() {
-        this.props.fetchQuizById(this.props.match.params.id);
-    }
+  const {fetchQuizById, retryQuiz, match} = props;
 
-    componentWillUnmount() {
-        this.props.retryQuiz();
-    }
+  useEffect(() => {
+    fetchQuizById(match.params.id);
+  }, [fetchQuizById, match]);
 
-    render() {
-        const {isFinished, results, quiz, loading} = this.props;
 
-        return(
-            <div className={cssCls.Quiz}>
-                <div className={cssCls.QuizWrapper}>
-                    <h1>Answer all questions</h1>
-                    {
-                        (loading || !quiz)
-                            ? <Loader/>
-                            : (isFinished)
-                                ? <FinishedQuiz
-                                    results={results}
-                                    quiz={quiz}
-                                    onRetry={this.props.retryQuiz}
-                                />
-                                : <ActiveQuiz
-                                    question={this.props.quiz[this.props.activeQuestion].question}
-                                    answers={this.props.quiz[this.props.activeQuestion].answers}
-                                    totalCountQuestion={this.props.quiz.length}
-                                    nextQuestionNum={this.props.activeQuestion + 1}
-                                    answerState={this.props.answerState}
-                                    onAnswerClick={this.props.answerClickHandler}
-                                />
-                    }
-                </div>
-            </div>
-        );
-    }
-}
+  useEffect(() => () => {
+    retryQuiz();
+  }, [retryQuiz]);
+
+  const {isFinished, results, quiz, loading} = props;
+
+  return (
+    <div className={cssCls.Quiz}>
+      <div className={cssCls.QuizWrapper}>
+        <h1>Answer all questions</h1>
+        {
+          (loading || !quiz)
+            ? <Loader/>
+            : (isFinished)
+            ? <FinishedQuiz
+              results={results}
+              quiz={quiz}
+              onRetry={props.retryQuiz}
+            />
+            : <ActiveQuiz
+              question={props.quiz[props.activeQuestion].question}
+              answers={props.quiz[props.activeQuestion].answers}
+              totalCountQuestion={props.quiz.length}
+              nextQuestionNum={props.activeQuestion + 1}
+              answerState={props.answerState}
+              onAnswerClick={props.answerClickHandler}
+            />
+        }
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
-    return {
-        activeQuestion: state.quiz.activeQuestion,
-        answerState: state.quiz.answerState,
-        isFinished: state.quiz.isFinished,
-        results: state.quiz.results,
-        quiz: state.quiz.quiz,
-        loading: state.quiz.loading
-    }
+  return {
+    activeQuestion: state.quiz.activeQuestion,
+    answerState: state.quiz.answerState,
+    isFinished: state.quiz.isFinished,
+    results: state.quiz.results,
+    quiz: state.quiz.quiz,
+    loading: state.quiz.loading
+  }
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        fetchQuizById: id => dispatch(fetchQuizById(id)),
-        answerClickHandler: answerId => dispatch(answerClickHandler(answerId)),
-        retryQuiz: () => dispatch(retryQuiz())
-    };
+  return {
+    fetchQuizById: id => dispatch(fetchQuizById(id)),
+    answerClickHandler: answerId => dispatch(answerClickHandler(answerId)),
+    retryQuiz: () => dispatch(retryQuiz())
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
